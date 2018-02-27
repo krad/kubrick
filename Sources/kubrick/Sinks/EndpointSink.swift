5ab5b62c8175cc22aeb3d8c7252777c5a6217cdb
krad <iam@krad.io>
2018-02-26 23:20:55 -0600

    Removing debug helpers

--- Sources/kubrick/Sinks/EndpointSink.swift
+++ Sources/kubrick/Sinks/EndpointSink.swift
@@ -12,7 +12,6 @@ public class EndpointSink: Sink<BinaryEncodable> {
     public override func push(input: BinaryEncodable) {
         do {
             let bytes = try BinaryEncoder.encode(input)
-            print(#function, "Writing: ", bytes.count)
             self.endpoint.write(Data(bytes))
         } catch let err {
             print("Could not encode input:", err)
